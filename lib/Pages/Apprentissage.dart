a7167736b8f308b425cb96dae23253807ee4abe9
unknown <traoreangefresnel@gmail.com>
2026-02-18 02:22:17 +0000

    ajouter

--- lib/Pages/Apprentissage.dart
+++ lib/Pages/Apprentissage.dart
@@ -18,11 +18,11 @@ class _ApprentissagePageState extends State<ApprentissagePage> {
           children: [
             Column(
               children: [
-                SizedBox(height: MediaQuery.of(context).size.height *0.035,),
+                SizedBox(height: MediaQuery.of(context).size.height *0.04,),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
-                    IconButton(onPressed: (){}, icon: Icon(CupertinoIcons.arrow_turn_up_left,color: Color(0xFF2E5AA6),)),
+                    IconButton(onPressed: (){}, icon: Icon(CupertinoIcons.arrow_turn_up_left,color: Color(0xFF2E5AA6),size: MediaQuery.of(context).size.width *0.07,)),
                     SizedBox(width: MediaQuery.of(context).size.width *0.04,),
                     Text("Beflêmi Kouadio",style: TextStyle(fontFamily: "Poppins",color: Color(0xFF2E5AA6),fontSize: MediaQuery.of(context).size.width *0.06),)
                     ,SizedBox(width: MediaQuery.of(context).size.width *0.2,),
@@ -46,13 +46,14 @@ class _ApprentissagePageState extends State<ApprentissagePage> {
                         )
                       ],
                     ),),
+                    SizedBox(height: MediaQuery.of(context).size.height *0.01,),
                     Container(
                       width:MediaQuery.of(context).size.width *0.5,
                       child: Column(
                         children: [
                           LinearProgressIndicator(
                             color: Colors.green,
-                            value: 0.2,
+                            value: 0.6,
                           )
                         ],
                       ),
@@ -60,21 +61,124 @@ class _ApprentissagePageState extends State<ApprentissagePage> {
                   ],
                 ),
                 Column(
-                  children: [
-                    Container(
-                      decoration: BoxDecoration(
 
-                      ),
-                      child: Row(
-                        children: [
-                          Container()
-                        ],
-                      ),
-                    )
+                  children: [
+                    SizedBox(height: MediaQuery.of(context).size.height *0.04,),
+                    Stack(
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                          ),
+                          child: Row(
+                            children: [
+                              Container(
+                                margin: EdgeInsets.only(left: MediaQuery.of(context).size.width *0.04),
+                                height: MediaQuery.of(context).size.height *0.13,
+                                width: MediaQuery.of(context).size.height *0.13,
+                                decoration: BoxDecoration(
+                                    color: Colors.blue,
+                                    borderRadius: BorderRadius.all(Radius.circular(MediaQuery.of(context).size.width *0.03))
+                                ),
+                              ),
+                              Column(
+                                children: [
+                                  Container(
+                                    width: MediaQuery.of(context).size.width *0.6,
+                                    alignment: AlignmentDirectional.topStart,
+                                    margin: EdgeInsets.only(left: MediaQuery.of(context).size.width *0.04),
+                                    child:Text("Salutations",style: TextStyle(color: Color(0xFF2E5AA6),fontFamily: "Poppins",fontSize: MediaQuery.of(context).size.width *0.04)),),
+
+
+                                  Container(
+                                    width: MediaQuery.of(context).size.width *0.6,
+                                    alignment: AlignmentDirectional.topStart,
+                                    margin: EdgeInsets.only(left: MediaQuery.of(context).size.width *0.04),
+                                    child: Text("Entrainement",style: TextStyle(color: Colors.black,fontFamily: "Poppins",fontSize: MediaQuery.of(context).size.width *0.04)),
+                                  ),
+
+                                  Container(
+                                    width: MediaQuery.of(context).size.width *0.6,
+                                    alignment: AlignmentDirectional.topStart,
+                                    margin: EdgeInsets.only(left: MediaQuery.of(context).size.width *0.04),
+                                    child: Text("Niveau recommandé",style: TextStyle(color: Color(0xFF2E5AA6),fontFamily: "Poppins",fontSize: MediaQuery.of(context).size.width *0.04),),
+                                  ),
+                                  Container(
+                                    width: MediaQuery.of(context).size.width *0.6,
+                                    alignment: AlignmentDirectional.topStart,
+                                    margin: EdgeInsets.only(left: MediaQuery.of(context).size.width *0.04),
+                                    child:Row(
+                                      children: [
+                                        Icon(Icons.star,color: CupertinoColors.systemYellow,),
+                                      ],
+                                      )),
+                                ],
+                              )
+                            ],
+                          ),
+                        ),
+
+
+
+
+                      ],
+                    ),
+                    SizedBox(height: MediaQuery.of(context).size.height *0.015,),
+                    Stack(
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                          ),
+                          child: Row(
+                            children: [
+                              Container(
+                                margin: EdgeInsets.only(left: MediaQuery.of(context).size.width *0.04),
+                                height: MediaQuery.of(context).size.height *0.13,
+                                width: MediaQuery.of(context).size.height *0.13,
+                                decoration: BoxDecoration(
+                                    color: Colors.blue,
+                                    borderRadius: BorderRadius.all(Radius.circular(MediaQuery.of(context).size.width *0.03))
+                                ),
+                              ),
+                              Column(
+                                children: [
+                                  Container(
+                                    width: MediaQuery.of(context).size.width *0.6,
+                                    alignment: AlignmentDirectional.topStart,
+                                    margin: EdgeInsets.only(left: MediaQuery.of(context).size.width *0.04),
+                                    child:Text("Présentations",style: TextStyle(color: Color(0xFF2E5AA6),fontFamily: "Poppins",fontSize: MediaQuery.of(context).size.width *0.04)),),
+                                  Container(
+                                    width: MediaQuery.of(context).size.width *0.6,
+                                    alignment: AlignmentDirectional.topStart,
+                                    margin: EdgeInsets.only(left: MediaQuery.of(context).size.width *0.04),
+                                    child: Text("Entrainement",style: TextStyle(color: Colors.black,fontFamily: "Poppins",fontSize: MediaQuery.of(context).size.width *0.04)),
+                                  ),
+
+                                  Container(
+                                    width: MediaQuery.of(context).size.width *0.6,
+                                    alignment: AlignmentDirectional.topStart,
+                                    margin: EdgeInsets.only(left: MediaQuery.of(context).size.width *0.04),
+                                    child: Text("Niveau recommandé",style: TextStyle(color: Color(0xFF2E5AA6),fontFamily: "Poppins",fontSize: MediaQuery.of(context).size.width *0.04),),
+                                  ),
+                                  Container(
+                                      width: MediaQuery.of(context).size.width *0.6,
+                                      alignment: AlignmentDirectional.topStart,
+                                      margin: EdgeInsets.only(left: MediaQuery.of(context).size.width *0.04),
+                                      child:Row(
+                                        children: [
+                                          Icon(Icons.star,color: CupertinoColors.systemYellow,),
+                                          Icon(Icons.star,color: CupertinoColors.systemYellow,),
+
+                                        ],
+                                      )),
+                                ],
+                              )
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
                   ],
                 )
-
-
               ],
             ),
             Container(
